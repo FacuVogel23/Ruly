c495d9ba7f5ba4992ba53aa4aa3420b149e49efb
kqDevs <facundovogel23@gmail.com>
2026-02-28 02:16:27 -0300

    Initial Commit

---
+++ Streakly/StreaklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreaklyApp.swift
+//  Streakly
+//
+//  Created by Facundo Vogel on 28/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct StreaklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
